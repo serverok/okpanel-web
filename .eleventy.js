cfdadee0b7c0347e9018b0e62a0d4eb822deb2da
Yujin Boby <admin@serverok.in>
2026-03-28 14:37:34 +0530

    converted site to 11ty

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/style.css");
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
+  eleventyConfig.addPassthroughCopy("src/sitemap.xml");
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+  };
+};
