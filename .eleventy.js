cec4e5731f448bc840d4769e0d7c4d99863c64b3
Yujin Boby <admin@serverok.in>
2026-04-08 11:02:25 +0530

    Revert "updated favicon"
    This reverts commit f6f583215db0de3387fd01ebf215dbe4f74e1e2d.

--- .eleventy.js
+++ .eleventy.js
@@ -4,8 +4,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/robots.txt");
   eleventyConfig.addPassthroughCopy("src/sitemap.xml");
   eleventyConfig.addPassthroughCopy("src/favicon.ico");
-  eleventyConfig.addPassthroughCopy("src/favicon.svg");
-  eleventyConfig.addPassthroughCopy("src/apple-touch-icon.png");
+  eleventyConfig.addPassthroughCopy("src/favicon_32x32.png");
 
   return {
     dir: {
