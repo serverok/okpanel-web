f6f583215db0de3387fd01ebf215dbe4f74e1e2d
Yujin Boby <admin@serverok.in>
2026-04-08 10:12:56 +0530

    updated favicon

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/robots.txt");
   eleventyConfig.addPassthroughCopy("src/sitemap.xml");
   eleventyConfig.addPassthroughCopy("src/favicon.ico");
-  eleventyConfig.addPassthroughCopy("src/favicon_32x32.png");
+  eleventyConfig.addPassthroughCopy("src/favicon.svg");
+  eleventyConfig.addPassthroughCopy("src/apple-touch-icon.png");
 
   return {
     dir: {
