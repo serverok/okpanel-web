977c25dda5ec2c70368c6a2c3a3094ed92198f01
Yujin Boby <admin@serverok.in>
2026-04-08 09:28:03 +0530

    added favicon

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
   eleventyConfig.addPassthroughCopy("src/sitemap.xml");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/favicon_32x32.png");
 
   return {
     dir: {
